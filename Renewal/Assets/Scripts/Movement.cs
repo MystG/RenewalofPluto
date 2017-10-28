3816db5f223f82c4e8fad24e0205c12b1c00956b
Matthew Carlson <mdcpanama22@gmail.com>
2017-10-28 16:07:33 -0400

    I added revolutions to the axis of the planets

--- Renewal/Assets/Scripts/Movement.cs
+++ Renewal/Assets/Scripts/Movement.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 	public float speed;
+	public float speed2;
 	// Update is called once per frame
 	void Update () {
 		Transform player = GameObject.Find("Sun").transform;
+		Transform current = transform;
 		transform.RotateAround(player.position, player.up, speed * Time.deltaTime);
+		transform.RotateAround(current.position, current.up, speed2 *Time.deltaTime);
 	}
 }

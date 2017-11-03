d92831347673a70a2019cd741a4c7f94ef747b21
khabane <seretsi@users.noreply.github.com>
2017-11-02 21:25:45 -0400

    prepull commit

--- Renewal/Assets/aiEasy.cs
+++ Renewal/Assets/aiEasy.cs
@@ -9,43 +9,69 @@ public class aiEasy : MonoBehaviour {
     public float enemyLookDistance;
     public float attackDistance;
     public float damping;
-    public Transform fpsTarget;
+    public float aggression; //delta time between each attack
+    public Transform playerTarget;
     Rigidbody theRigidBody;
     Renderer myRender;
+    static Animator anim;
     
     // Use this for initialization
 	void Start () {
         myRender = GetComponent<Renderer>();
         theRigidBody = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
         //attack radius logic  
-        fpsTargetDistance = Vector3.Distance(fpsTarget.position, transform.position);
+        fpsTargetDistance = Vector3.Distance(playerTarget.position, transform.position);
         if (fpsTargetDistance<enemyLookDistance)
         {
             myRender.material.color = Color.yellow;
             lookAtPlayer();
             //add "Do I hear something?" logic
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", true);
         }
         else if (fpsTargetDistance < attackDistance)
         {
             myRender.material.color = Color.red;
             enemyAttack();
         }
+        else
+        {
+            wonder();
+        }
 	}
 
     void lookAtPlayer()
     {
-        Quaternion rotation = Quaternion.LookRotation(fpsTarget.position - transform.position);
+        Quaternion rotation = Quaternion.LookRotation(playerTarget.position - transform.position);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime + damping);
     }
 
     void enemyAttack()
     {
+        float currentTime = 0.0f;
         //define attack movement pattern
         //define attack rate 
+        if (Time.time > currentTime)
+        {
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isAttacking", true);
+            currentTime = Time.time + aggression;
+            GameObject clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+        }
         // make controllable for difficulty
+    }
+
+    void wonder()
+    {
+        //move around navmesh or patrol path
+        anim.SetBool("isIdle", true);
+        anim.SetBool("isWalking", false);
+        anim.SetBool("isAttacking", false);
     }
 }

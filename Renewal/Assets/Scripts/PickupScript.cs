7b4a85a8e42014255798cfdb7ac0b9d586d46086
Alexander Christner <christner.alex@gmail.com>
2017-11-08 01:45:37 -0500

    created script for puckup objects to display text

--- Renewal/Assets/Scripts/PickupScript.cs
+++ Renewal/Assets/Scripts/PickupScript.cs
@@ -1,23 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickupScript : MonoBehaviour {
 
     public string message;
+    public float activeDistance;
+    public float textDuration;
+    public Vector2 rectSize;
+    public Color textCol;
+    public GameObject player;
+
+    private bool visible;
+    private float dist;
+
+    private bool activate;
+    private float timer;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        visible = false;
+        dist = Mathf.Infinity;
+        activate = false;
+        timer = 0;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
 
-    private void OnMouseDown()
+        visible = GetComponent<Renderer>().isVisible;
+        dist = Vector3.Distance(transform.position, player.transform.position);
+
+        if (visible && dist <= activeDistance)
+        {
+            activate = true;
+        }
+
+        if (activate)
+        {
+            timer += Time.deltaTime;
+            if(timer >= textDuration)
+            {
+
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    private void OnGUI()
     {
-        Destroy(this.gameObject);
+        if (activate)
+        {
+            GUIStyle style = new GUIStyle();
+            style.normal.textColor = textCol;
+            Vector3 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+            GUI.Label(new Rect(screenPos, rectSize), message, style);
+        }
     }
 }
